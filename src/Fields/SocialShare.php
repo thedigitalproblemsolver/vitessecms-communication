d2b79dd52ea9759cf216283e8e2775416cdea682
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-06-25 00:28:43 +0200

    Sent first hello world tweet

--- src/Fields/SocialShare.php
+++ src/Fields/SocialShare.php
@@ -2,21 +2,8 @@
 
 namespace VitesseCms\Communication\Fields;
 
-use VitesseCms\Datafield\Models\Datafield;
-use VitesseCms\Database\AbstractCollection;
 use VitesseCms\Datafield\AbstractField;
-use VitesseCms\Form\AbstractForm;
-use VitesseCms\Form\Models\Attributes;
 
 class SocialShare extends AbstractField
 {
-    public function buildItemFormElement(
-        AbstractForm $form,
-        Datafield $datafield,
-        Attributes $attributes,
-        AbstractCollection $data = null
-    )
-    {
-        $form->addToggle('Share on Twitter', $datafield->getCallingName().'_twitter', $attributes);
-    }
 }
