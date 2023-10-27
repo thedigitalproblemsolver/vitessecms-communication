e230df852b22a75f3c19270ba71a89fc020ac695
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2023-10-27 21:57:09 +0200

    Code cleanup

--- src/Listeners/Controllers/AdminemailControllerListener.php
+++ src/Listeners/Controllers/AdminemailControllerListener.php
@@ -8,33 +8,8 @@ use Phalcon\Events\Event;
 use VitesseCms\Admin\Forms\AdminlistFormInterface;
 use VitesseCms\Communication\Controllers\AdminemailController;
 
-class AdminemailControllerListener
+final class AdminemailControllerListener
 {
-    /*public function adminListItem(Event $event, AdminemailController $controller, Email $email): void
-    {
-        $email->setAdminListExtra(
-            $controller->view->renderTemplate(
-                'emailAdminListItem',
-                $controller->configuration->getVendorNameDir() . 'communication/src/Resources/views/admin/',
-                ['email' => $email]
-            )
-        );
-
-        if (PermissionUtils::check(
-            $controller->user,
-            $controller->view->getVar('aclModulePrefix') . 'communication',
-            'adminemail',
-            'sendPreview'
-        )) :
-            $email->setExtraAdminListButtons(
-                Tag::linkTo([
-                    'action' => '/Admin/communication/adminemail/sendPreview/' . $email->getId(),
-                    'class' => 'fa fa-envelope',
-                ])
-            );
-        endif;
-    }*/
-
     public function adminListFilter(Event $event, AdminemailController $controller, AdminlistFormInterface $form): void
     {
         $form->addText(
